79f16630e2a5dfceeaed9caef72a74ecb5a3eab4
SI-Tanbir <si017490@gmail.com>
2024-11-18 12:32:04 +0600

    adding go back features buttons

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,14 +1,23 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useNavigate } from "react-router-dom"
 
 
 function PostDetails() {
 
     const data=useLoaderData();
+    const navigate =useNavigate();
+    function goBack(){
+
+        navigate(-1);
+        
+    }
+
+
   return (
     <div className="m-8 bg-slate-200 border">
 
         <h3>Title:{data.title}</h3>
         <p>body:{data.body}</p>
+        <button onClick={goBack}>Go Back</button>
 
     </div>
   )
