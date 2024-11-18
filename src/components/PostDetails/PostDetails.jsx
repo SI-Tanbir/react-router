c9e0606e906c9490a27b9f92b7fd14eefd49c3c9
SI-Tanbir <si017490@gmail.com>
2024-11-18 12:07:19 +0600

    fix the issue

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,17 +1,6 @@
 import { useLoaderData } from "react-router-dom"
 
 
-// body
-// : 
-// "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-// id
-// : 
-// 2
-// title
-// : 
-// "qui est esse"
-// userId
-// :
 function PostDetails() {
 
     const data=useLoaderData();
